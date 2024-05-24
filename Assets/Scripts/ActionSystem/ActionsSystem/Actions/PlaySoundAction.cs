e5adb7f238deffec2fc300b405bfe5b57c181131
Artur Pashchenko <144344189+Chelentanick@users.noreply.github.com>
2024-05-24 21:03:32 +0200

    New Main menu

--- Assets/Scripts/ActionSystem/ActionsSystem/Actions/PlaySoundAction.cs
+++ Assets/Scripts/ActionSystem/ActionsSystem/Actions/PlaySoundAction.cs
@@ -3,8 +3,18 @@ namespace ActionSystem {
     public class PlaySoundAction : ActionBase
     {
         [SerializeField] AudioSource _sound;
+        [Header("Without AudioSource")]
+        [SerializeField] bool _withoutAudioSource;
+        [SerializeField] AudioClip _clip;
+        [SerializeField] float _volume;
+        
         public override void Execute(object data = null)
         {
+            if (_withoutAudioSource) 
+            {
+                AudioSource.PlayClipAtPoint(_clip,transform.position,_volume);
+            }
+            else
            _sound.Play();
         }
     }
